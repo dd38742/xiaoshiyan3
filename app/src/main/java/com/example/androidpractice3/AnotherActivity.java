1d2f843a852d2d184d63b615629d3b647d1bc024
lyx <1986270140@qq.com>
2019-11-07 23:03:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidpractice3/AnotherActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidpractice3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+    }
+}
